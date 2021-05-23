fe8cfa3ebf74ac3c3937b632057553708d04a2ae
EmilsKuz <kemils1323@gmail.com>
2021-05-24 01:53:52 +0300

    Papildinats doc

--- UnityFpsGP/Assets/Scripts/TargetsLeft.cs
+++ UnityFpsGP/Assets/Scripts/TargetsLeft.cs
@@ -13,15 +13,12 @@ public class TargetsLeft : MonoBehaviour
     {
         targetCount = 5;
     }
-    // Update is called once per frame
     void Update()
     {
-        //print("Target Count is" + targetCount); //indebugger you shout see 4 because thats what we told it to be in the variable
         targetText.text = "Targets left: " + targetCount;
 
         if (targetCount < 1)
         {
-            
             Timer.Finnish();
         }
     }
